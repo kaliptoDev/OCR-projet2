930b9d1f28f0d5ba5e53b37b4306db2383fbbced
kaliptoDev <baptiste.andre17@gmail.com>
2023-01-31 11:36:23 +0100

    File Cleaning / CSS Rules addition /Login Page imp
    Login Page started: main elements hidden when Login selected
    Nav Li Font Weight According to selection
    Page cleaned of useless comments after succesful tests
    Triggers function organized / Event Listeners added, working as expected

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/js/script.js
@@ -2,15 +2,9 @@ const fetchWorks = async function (workId) {
     const res = await fetch('http://localhost:5678/api/works', { method: 'GET' });
     const worksList = await res.json();
     if (workId > 0) {
-        // worksList.filter(work => work.category.id === workId);
-        // const worksListFiltered = worksList.filter(work => work.categoryId === workId);
-        // console.log('worksList: ' + JSON.stringify(worksList));
-        // console.log('Id :' + workId);
         return worksList.filter(work => work.categoryId === workId);
     }
     else {
-        // worksListFiltered = worksList;
-        // console.log('nothing changed from the original list')
         return worksList;
     }
 
@@ -29,43 +23,93 @@ const generateWork = function (work) {
     document.getElementById('work_' + work.id).appendChild(workElementFigureImg);
     document.getElementById('work_' + work.id).appendChild(captionWork);
 
-    // return workElement;
 }
 
 const generateWorks = async function (workId) {
     const works = await fetchWorks(workId);
-    // console.log('works: ');
-    // console.log(works);
+
     document.querySelector('.gallery').innerHTML = '';
 
     for (let work of works) {
         generateWork(work);
-        // console.log('work: ' + JSON.stringify(work.categoryId));
-    }
-
-    // console.log('works: ' + JSON.stringify(works));
+     }
 
     return works;
 }
 
+const triggers = function () {
+    triggerContact();
+    triggerFilter();
+    triggerLogin();
+    triggerWork();
+}
+
+const triggerFilter = function () {
+    const filtersInput = document.querySelectorAll('.filter input');
+    for (let filter of filtersInput) {
+        filter.addEventListener('change', function (event) {
+            // console.log('filter: ' + event.target.id);
+            switch (event.target.id) {
+                case 'radio0': generateWorks(0); break;
+                case 'radio1': generateWorks(1); break;
+                case 'radio2': generateWorks(2); break;
+                case 'radio3': generateWorks(3); break;
+            }
+            // generateWorks();
+        });
+    }
+
+}
+
+const triggerContact = function () {
+    const contactTrigger = document.querySelector('.contact');
+    contactTrigger.addEventListener('click', function (event) {
+        console.log('contact');
+        document.querySelector('.worksGallery').style.fontWeight = '400';
+        document.querySelector('.login').style.fontWeight = '400';
+        document.querySelector('.contact').style.fontWeight = '600';
+        showWorks();
+        document.querySelector('#contact').scrollIntoView({behavior: 'smooth'}, true);
+    });
+
+
+}
+
+const triggerWork = function () {
+    const backToWorks = document.querySelector('.worksGallery');
+    backToWorks.addEventListener('click', function (event) {
+        console.log('works');
+        document.querySelector('.worksGallery').style.fontWeight = '600';
+        document.querySelector('.login').style.fontWeight = '400';
+        document.querySelector('.contact').style.fontWeight = '400';
+        showWorks();
+    });
+
+}
+
+const triggerLogin = function () {
+    const loginTrigger = document.querySelector('.login');
+    loginTrigger.addEventListener('click', function (event) {
+        console.log('login');
+        hideZone('#introduction');
+        hideZone('#portfolio');
+        hideZone('#contact');
+        document.querySelector('.worksGallery').style.fontWeight = '400';
+        document.querySelector('.login').style.fontWeight = '600';
+        document.querySelector('.contact').style.fontWeight = '400';
+    });
+
+}
+
+const showWorks = function () {
+    document.querySelector('#portfolio').style.display = 'flex';
+    document.querySelector('#introduction').style.display = 'flex';
+    document.querySelector('#contact').style.display = 'block';
+}
+
+const hideZone = function (zone) {
+    document.querySelector(zone).style.display = 'none';
+}
 //MAIN CALLS
 const works = generateWorks();
-//.then(works => console.log('works: ' + JSON.stringify(works)))
-// const filters = document.querySelectorAll('.filter');
-
-const filtersInput = document.querySelectorAll('.filter input');
-
-// console.log('filters: ' + JSON.stringify(filtersInput));
-
-for (let filter of filtersInput) {
-    filter.addEventListener('change', function (event) {
-        // console.log('filter: ' + event.target.id);
-        switch (event.target.id) {
-            case 'radio0': generateWorks(0); break;
-            case 'radio1': generateWorks(1); break;
-            case 'radio2': generateWorks(2); break;
-            case 'radio3': generateWorks(3); break;
-        }
-        // generateWorks();
-    });
-}
+triggers();
